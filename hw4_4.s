8b695135b9ea6a94a882b9419decca50e4aa871c
Conrad Mossl <conradmossl@gmail.com>
2014-10-13 12:35:53 -0400

    NA

--- hw4_4.s
+++ hw4_4.s
@@ -55,6 +55,34 @@ main:
 	#carry out the operation labled by $v0
 	syscall
 
+	#set the syscall to read in an integer
+	li $v0, 5
+	#carry out the operation labled by $v0
+	syscall
+
+	#store the second input integer into $t0
+	add $t0, $zero, $v0
+
+	#set the syscall to read in an integer
+	li $v0, 5
+	#carry out the operation labled by $v0
+	syscall
+
+	#store the first input integer into $t1
+	add $t1, $zero, $v0
+
+	#set the syscall to read in a character
+	li $v0, 12
+	#carry out the operation labled by $v0
+	syscall
+
+	#take the data from $v0 and store it in the space allocated for oper
+	sw $v0, oper
+	#take the data from oper and store it in $t3
+	lw $t2, oper
+
+	lui 
+
 	#loop until the user sends "n"
 Loop:
 
