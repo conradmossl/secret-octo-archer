615d38fab869e6dae9caff7404728c295ff8a61d
Conrad Mossl <conradmossl@gmail.com>
2014-10-13 13:40:36 -0400

    Addition
    Finished the addition subroutine

--- hw4_4.s
+++ hw4_4.s
@@ -1,14 +1,19 @@
 #the following information will be data
 	.data
 
+####
+	.globl main
+
+
 #the following variables will have the corresponding number of bytes
 #set aside for use
+oper: .space 4
 
 #the following variables will by set to the following data
 heading: .asciiz "Welcome to SPIM Calculator 1.0!"
-first: .asciiz "Enter the first number"
-second: .asciiz "Enter the first number"
-operation: .asciiz "Enter the operation (+,-,*,/), the press enter key: ="
+first: .asciiz "\nEnter the first number "
+second: .asciiz "\nEnter the second number "
+operation: .asciiz "\nEnter the operation (+,-,*,/), the press enter key: ="
 calculation: .asciiz "Another Calculation"
 terminate: .asciiz "Calculator Terminated"
 
@@ -17,31 +22,51 @@ terminate: .asciiz "Calculator Terminated"
 
 #function for addition
 Add:
-	
+	add $s3, $s1, $s0
+	# jal Answer
+
+	add $a0, $zero, $s3
+
+	#set the syscall to print out an integer
+	li $v0, 1
+	#carry out the operation labled by $v0
+	syscall
+
+	jr $ra
 
 #function for multiplication
 #a0 X a1 = 
 
-Multiply: 
-	# a counter to keep track of how many times we've iterated
-	# through the loop
-	add $s0, $zero, $zero
-	# a variable to store the result of the operation
-	add $s1, $zero, $zero	
-	ja Loop
-Return:
-	# store result on stack
-	# adjust stack pointer accordingly
+# Multiply: 
+# 	# a counter to keep track of how many times we've iterated
+# 	# through the loop
+# 	add $s0, $zero, $zero
+# 	# a variable to store the result of the operation
+# 	add $s1, $zero, $zero	
+# 	ja Loop
+# Return:
+# 	# store result on stack
+# 	# adjust stack pointer accordingly
 
-Loop:
-	add $s0, $s0, $a0
-	addi $s0, 1
-	beq $s0, $s1, Return
-	ja Loop
+# Loop:
+# 	add $s0, $s0, $a0
+# 	addi $s0, 1
+# 	beq $s0, $s1, Return
+# 	ja Loop
 
 #function for division
 
+###
+# Answer:
+	
+# 	add $a0, $zero, $s3
 
+# 	#set the syscall to print out an integer
+# 	li $v0, 1
+# 	#carry out the operation labled by $v0
+# 	syscall
+
+# 	jr $ra
 
 #the following will be the main program
 main:
@@ -49,11 +74,17 @@ main:
 	#title section
 		#will happen regardless of first equation
 	
-	addi $sp, $sp, -12
+	#make room for 2 integers and a character on the stack
+	addi $sp, $sp, -16
 	
-	sw $s0, 8($sp)
-	sw $s1, 4($sp)
-	sw $s2, 0($sp)
+	#load variables to these allotments
+	sw $s0, 12($sp)
+	sw $s1, 8($sp)
+	sw $s2, 4($sp)
+	sw $s3, 0($sp) 
+
+	#set the syscall to print out a string
+	li $v0, 4
 	#set a point in the memory for the string
 	la $a0, heading
 	#carry out the operation labled by $v0
@@ -61,6 +92,8 @@ main:
 
 	#set the syscall to print out a string
 	li $v0, 4
+	#set a point in the memory for the string
+	la $a0, first
 	#carry out the operation labled by $v0
 	syscall
 
@@ -69,16 +102,30 @@ main:
 	#carry out the operation labled by $v0
 	syscall
 
-	#store the second input integer into $t0
+	#store the first input integer into $t0
 	add $s0, $zero, $v0
 
+	#set the syscall to print out a string
+	li $v0, 4
+	#set a point in the memory for the string
+	la $a0, second
+	#carry out the operation labled by $v0
+	syscall
+
 	#set the syscall to read in an integer
 	li $v0, 5
 	#carry out the operation labled by $v0
 	syscall
 
-	#store the first input integer into $t1
+	#store the second input integer into $t1
 	add $s1, $zero, $v0
+
+	#set the syscall to print out a string
+	li $v0, 4
+	#set a point in the memory for the string
+	la $a0, operation
+	#carry out the operation labled by $v0
+	syscall
 
 	#set the syscall to read in a character
 	li $v0, 12
@@ -90,27 +137,23 @@ main:
 	#take the data from oper and store it in $t3
 	lw $s2, oper
 
-	lui $t0, '+'
+	beq $s2, '+', Add
 
-	beq $s2, $t0, Add
-
+	# jal End
 
 	#loop until the user sends "n"
-Loop:
+# End:
+# 	#ending section
 
+# 	#set a point in the memory for the string
+# 	la $a0, terminate
+# 	#carry out the operation labled by $v0
+# 	syscall
 
-End:
-	#ending section
+# 	#set the syscall to print out a string
+# 	li $v0, 4
+# 	#carry out the operation labled by $v0
+# 	syscall
 
-	#set a point in the memory for the string
-	la $a0, terminate
-	#carry out the operation labled by $v0
-	syscall
-
-	#set the syscall to print out a string
-	li $v0, 4
-	#carry out the operation labled by $v0
-	syscall
-
-	#return
-	jr $ra
+# 	#return
+# 	jr $ra
